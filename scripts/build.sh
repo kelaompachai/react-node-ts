591678414b042fd6e0b0107853544f3ad9cc8057
kelaompachai <141376761+kelaompachai@users.noreply.github.com>
2024-09-06 22:50:01 -0400

    split tsconfig into two, one for view and one for controller

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 
-tsc
+tsc -p app/controller/tsconfig.json
+# webpack something something something
+
+
+# my build is going to compile server typescript and emit it
+# then bundle frontend typescript with webpack and emit that
+# then npm start will run the server
