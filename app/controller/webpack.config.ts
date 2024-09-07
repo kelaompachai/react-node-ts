85c02285a86ccca1579ab40d84627750b7200144
kelaompachai <141376761+kelaompachai@users.noreply.github.com>
2024-09-07 16:21:22 -0400

    get webpack to compile

--- app/controller/webpack.config.ts
+++ app/controller/webpack.config.ts
@@ -1,16 +1,36 @@
 import path from 'path';
 
+const ENTRY: string = path.join(__dirname, '..', 'view', 'build', 'index.js');
+console.log('ENTRY:', ENTRY);
+const EXIT: string = path.join(__dirname, '..', 'view', 'dist')
+console.log('EXIT:', EXIT);
+
+
 // may need to install @types/webpack for this to have any quality type checking
 const webpackOptions = (env: any, argv: any): any => {
   console.log(env);
   console.log(argv);
 
   return {
-    entry: path.join(__dirname, '..', 'view', 'build', 'index.js'),
+    entry: ENTRY,
     output: {
-      path: path.join(__dirname, '..', 'view', 'dist'),
+      path: EXIT,
       filename: 'bundle.js',
       clean: true
+    },
+    module: {
+      rules: [
+        {
+          test: /.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        }
+      ]
     }
   };
 
