49538b6068a80857640ed2e6f27494846e667c84
kelaompachai <141376761+kelaompachai@users.noreply.github.com>
2024-09-10 14:21:59 -0400

    get tailwind to work

--- app/view/component.tsx
+++ app/view/component.tsx
@@ -2,10 +2,10 @@ import React, { ReactElement } from 'react';
 
 const Component = (): ReactElement => {
   return (
-    <>
-      <div>Hello React</div>
-      <div>Hello Webpack</div>
-    </>
+    <div>
+      <div className="text-red-500">Hello React</div>
+      <div className="text-blue-500">Hello Webpack</div>
+    </div>
   );
 };
 
